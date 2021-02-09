69aac85aed9975a8d5fd20f8480ec6ee34c41677
Dave Jeffers <72349891+zdjeffers@users.noreply.github.com>
2021-02-08 21:42:08 -0500

    finished index.js file. updated generatemarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,29 +1,64 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
-
-const writeFileAsync = util.promisify(fs.writeFile);
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [title,
-descriptioon,
-installation,
-usage,
-contribution,
-test,
-license(list),
-github,
-email,
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "Title of Project:"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Description of Project:"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Project Usage:"
+    },
+    {
+        type: 'input',
+        name: 'Contribution',
+        message: "Your Project Contribution:"
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "Tests Run:"
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: "Licenses used:",
+        choices: ["MIT", "ISC", "Apache 2.0", "MPL-2.0"]
+        //Need to fix this, choices do not show up
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Your Github Account:"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Your Email:"
+    },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeToFile
-}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer.prompt(questions).then(data => {
+        console.log(data);
+        let newReadMe = generateMarkdown(data)
+        fs.writeFile("README.md", newReadMe, (err) =>
+            err ? console.log(err) : console.log('Congratulations! You have created your README!')
+        );
+    })
+}
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,16 +1,24 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+if (license === "None"){
+  return "None"
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+if(license === "MIT"){
+  return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+if (license === "Apache"){
+ return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+} 
 
-// TODO: Create a function to generate markdown for README
+
+
+if(license === "Eclipse"){
+  return `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+}
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
 
